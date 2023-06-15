0dbd57911a662628644bbfc2d3ce684931df127e
pratikash <pratikashpanda.1234@gmail.com>
2023-06-15 10:51:12 +0530

    cookie methods added

--- controllers/user.js
+++ controllers/user.js
@@ -12,7 +12,11 @@ export const myProfile = (req, res, next) => {
 export const logout = (req, res, next) => {
   req.session.destroy((err) => {
     if (err) return next(err);
-    res.clearCookie("connect.sid");
+    res.clearCookie("connect.sid", {
+      secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
+    });
     res.status(200).json({
       message: "Logged OUT",
     });
@@ -38,9 +42,9 @@ export const getAdminStats = asyncError(async (req, res, next) => {
 
   let totalIncome = 0;
 
-  orders.forEach((i)=>{
+  orders.forEach((i) => {
     totalIncome += i.totalAmount;
-  })
+  });
 
   res.status(200).json({
     success: true,
