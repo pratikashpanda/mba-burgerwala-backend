423a0ba6ab9cc9cf9fd46915e33c4e73f1b1fc6a
pratikash <pratikashpanda.1234@gmail.com>
2023-09-21 00:59:37 +0530

    new auth changes

--- app.js
+++ app.js
@@ -21,29 +21,23 @@ app.use(
     resave: false,
     saveUninitialized: false,
 
-    cookie:{
-      secure:process.env.NODE_ENV === 'development'? false : true,
-      httpOnly:process.env.NODE_ENV === 'development'? false : true,
-      sameSite:process.env.NODE_ENV === 'development'? false : "none"
-    }
+    cookie: {
+      secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
+    },
   })
 );
 app.use(cookieParser());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use(cors());
 
 app.use(passport.authenticate("session"));
 app.use(passport.initialize());
 app.use(passport.session());
-app.enable("trust proxy")
+app.enable("trust proxy");
 
 connectPassport();
 
